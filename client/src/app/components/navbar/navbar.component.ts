66894963135c724d9191491862b6b187db83b364
Blackjuker <ngabgnaarnaud2@gmail.com>
2024-08-24 18:27:26 +0200

    ajout du bouton login et register. et creation de la page home page

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule,MatButtonModule,MatIconModule],
+  imports: [RouterLink,MatToolbarModule,MatButtonModule,MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
