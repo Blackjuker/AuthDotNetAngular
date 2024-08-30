8e488dd5cf347fab9285b74c146eb036a93c8af9
Blackjuker <ngabgnaarnaud2@gmail.com>
2024-08-30 15:03:44 +0200

    install jwt-decode library and implements login, logout, isTokenExpired is loggedIn and logOut

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -4,6 +4,10 @@ import { LoginRequest } from '../interfaces/login-request';
 import { map, Observable } from 'rxjs';
 import { AuthResponse } from '../interfaces/auth-response';
 import { HttpClient } from '@angular/common/http';
+import {jwtDecode} from 'jwt-decode';
+
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -23,4 +27,32 @@ export class AuthService {
       })
     );
   }
+
+  isLoggedIn  = ():boolean => {
+    const token = this.getToken();
+    if(!token) return false;
+
+    return !this.isTokenExpired();
+  }
+
+  
+
+
+
+  private isTokenExpired(){
+    const token = this.getToken();
+    if(!token) return true;
+    const decoded = jwtDecode(token);
+    const isTokenExpired = Date.now() >= decoded['exp']! * 1000;
+    if(isTokenExpired) this.logout();
+    return isTokenExpired;
+  }
+
+  logout=():void=>{
+    localStorage.removeItem(this.tokenKey);
+  }
+
+  private getToken = ():string|null => localStorage.getItem(this.tokenKey) || '';
+
+
 }
